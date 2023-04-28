a8ec2ccd6874b8b2ac8cee169be12f15d203118f
Derek Truesdale <dtruesdale37@gmail.com>
2023-04-28 14:04:24 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const people=['Guadalupe','Ollie', 'Aki']
+function writeCards(people, s='suprise'){
+    let thankYouCards= []
+    for(let a=0;a<people.length;a++){
+        thankYouCards.push(`Thank you, ${people[a]}, for the wonderful ${s} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(){
+    let timer=10
+    while (timer>-1){
+        console.log(timer)
+        timer--
+    }
+}
